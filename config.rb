a8fe9433ed302fa8df5e88861d87a2fd4578ab05
Martin <martin@mwi.se>
2014-02-09 20:04:03 +0100

    Removed old sass folder

--- config.rb
+++ config.rb
@@ -1,9 +1,10 @@
 # Require any additional compass plugins here.
 
+
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "/"
-sass_dir = "sass"
+css_dir = "css"
+sass_dir = "scss"
 images_dir = "images"
 javascripts_dir = "javascripts"
 
