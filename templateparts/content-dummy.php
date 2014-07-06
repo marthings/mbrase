8a6324516138a99910613bc0dff025e4bd0c69ae
Martin Berggren <martin@mwi.se>
2014-07-06 18:56:15 +0200

    Content now separated from loops

--- templateparts/content-dummy.php
+++ templateparts/content-dummy.php
@@ -2,6 +2,6 @@
 
 Your code here whatever it might be. Then use it in your page-templates by getting it with:
 
-<?php // get_template_part( 'templateparts/templatepart-dummy'); ?>
+<?php // get_template_part( 'templateparts/content-dummy'); ?>
 
 -->
