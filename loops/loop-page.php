a95839ef6d502720df7678fd67d42657c56e75a8
Martin Berggren <martin@mwi.se>
2014-07-06 15:53:09 +0200

    resolved #1
    Removed heading markup

--- loops/loop-page.php
+++ loops/loop-page.php
@@ -1,4 +1,4 @@
 <?php while (have_posts()) : the_post(); ?>
-    <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+    <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
    	<?php the_content(); ?>
 <?php endwhile;?>
