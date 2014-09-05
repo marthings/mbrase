5974044b46969592a95960c747cd1fd1ce263389
Martin Berggren <martin@mwi.se>
2014-09-05 15:46:33 +0200

    Updated header with logo

--- templateparts/navigation.php
+++ templateparts/navigation.php
@@ -10,13 +10,19 @@
 <div class="navbar">
 	<div class="container">
 		<div class="navbar-header">
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#main-navbar">
 					<span class="sr-only">Toggle navigation</span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
+			<?php
+			$has_logo =  get_field('site_logo', 'option');
+			if ($has_logo) { ?>
+				<a class="navbar-brand has-logo" href="/"><img src="<?php the_field('site_logo', 'option'); ?>"></a>
+			<?php } else { ?>
+				<a class="navbar-brand" href="/"><?php bloginfo('name'); ?></a>
+			<?php } ?>
 		</div>
 		 <?php
 			wp_nav_menu( array(
@@ -25,8 +31,8 @@
 				'depth'             => 2,
 				'container'         => 'div',
 				'container_class'   => 'collapse navbar-collapse',
-				'container_id'      => 'bs-example-navbar-collapse-1',
-				'menu_class'        => 'nav navbar-nav',
+				'container_id'      => 'main-navbar',
+				'menu_class'        => 'nav navbar-nav navbar-right',
 				'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
 				'walker'            => new wp_bootstrap_navwalker())
 			);
